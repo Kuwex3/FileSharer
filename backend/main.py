98e6bfcb67eabdc384f48b0eb63788877ae74db1
Kuwex3 <vizinovskiy@gmail.com>
2026-07-24 22:52:51 +0300

    edit router name

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from backend.endpoints.upload_file import router as upRouter
+from backend.endpoints.upload_file import router as uploadRouter
 
 app = FastAPI()
 
-app.include_router(upRouter)
+app.include_router(uploadRouter)
 
 @app.get("/")
 async def main():
