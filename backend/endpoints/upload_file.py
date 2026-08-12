5e3b099174da38eab1359d3c7c4e4e72dd3b5109
Kuwex3 <vizinovskiy@gmail.com>
2026-08-12 22:06:22 +0300

    edit upload method

--- backend/endpoints/upload_file.py
+++ backend/endpoints/upload_file.py
@@ -1,13 +1,23 @@
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, Request, Response
 
 router = APIRouter()
 
+
 @router.post("/upload")
-async def test_router(user_file: UploadFile):
-    if user_file.size < 100000000000000000000000000000000000:
-        data = await user_file.read()
-        file_name = user_file.filename
-        with open(f"backend/test_storage/{file_name}", "wb") as file:
-            file.write(data)
-    else:
-        return {"access": "denied"}
+async def test_router(request: Request):
+    total_bytes = 0
+    with open("./backend/test_storage/12.mp3", "ab") as file:
+        async for chunk in request.stream():
+            file.write(chunk)
+            print(f"collected chunks: {total_bytes}")
+        return {"test"}
+
+# @router.post("/upload")
+# async def test_router(user_file: UploadFile):
+#     if user_file.size < 100000000000000000000000000000000000:
+#         data = await user_file.read()
+#         file_name = user_file.filename
+#         with open(f"backend/test_storage/{file_name}", "wb") as file:
+#             file.write(data)
+#     else:
+#         return {"access": "denied"}
