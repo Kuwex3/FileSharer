94bdcbb814f2daee9e282c881d7f202fc4f194d6
KuweK <vizinovskiy@gmail.com>
2026-08-29 16:10:37 +0300

    add primitive download func

--- backend/endpoints/download_file.py
+++ backend/endpoints/download_file.py
@@ -1,7 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.responses import FileResponse
+
+from backend.db_functions.main import get_async_session
+from backend.db_functions.models import File
+
+from sqlmodel import select
 
 router = APIRouter()
 
 @router.get("/download")
-async def download_file(file_id: str | None = None):
-    return {file_id}
+async def download_file(file_id: str | None = None, session = Depends(get_async_session)):
+    if file_id == None:
+        return {"No file to response"}
+    
+    stmnt = select(File).where(File.id == file_id)
+    result = await session.exec(stmnt)
+    
+    file_name = result.all()[0].file_name
+    
+    file = FileResponse(
+        path=f"./backend/test_storage/{file_name}",
+        filename=file_name
+    )
+    return file
